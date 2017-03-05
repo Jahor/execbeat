c6e761ce4d84b4973751a33c4fcacd7942710c28
Egor Leonenko <jagor@leonenko.info>
2017-03-05 18:11:37 +1300

    Update tests

--- beater/execevent_test.go
+++ beater/execevent_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestHttpEventToMapStr(t *testing.T) {
+func TestExecEventToMapStr(t *testing.T) {
 	now := time.Now()
 	fields := make(map[string]string)
 	fields["field1"] = "value1"
@@ -20,7 +20,7 @@ func TestHttpEventToMapStr(t *testing.T) {
 	event.Fields = fields
 	event.DocumentType = "test"
 	event.ReadTime = now
-	event.Exec = command
+	event.Exec = &command
 	mapStr := event.ToMapStr()
 	_, fieldsExist := mapStr["fields"]
 	assert.True(t, fieldsExist)
@@ -28,9 +28,44 @@ func TestHttpEventToMapStr(t *testing.T) {
 	assert.True(t, execExist)
 	assert.Equal(t, "test", mapStr["type"])
 	assert.Equal(t, common.Time(now), mapStr["@timestamp"])
+
+	_, lineExist := mapStr["line"]
+	assert.False(t, lineExist)
 }
 
-func TestHttpEventToMapStrWIthEmptyFields(t *testing.T) {
+func TestExecEventWithLineToMapStr(t *testing.T) {
+	now := time.Now()
+	fields := make(map[string]string)
+	fields["field1"] = "value1"
+	fields["field2"] = "value2"
+	command := Line{}
+	command.Command = "foo"
+	command.Line = "test"
+	command.Source = "test"
+	command.LineNumber = 1
+
+	event := ExecEvent{}
+	event.Fields = fields
+	event.DocumentType = "test"
+	event.ReadTime = now
+	event.Line = &command
+
+	mapStr := event.ToMapStr()
+
+	_, fieldsExist := mapStr["fields"]
+	assert.True(t, fieldsExist)
+
+	_, execExist := mapStr["exec"]
+	assert.False(t, execExist)
+
+	_, lineExist := mapStr["line"]
+	assert.True(t, lineExist)
+
+	assert.Equal(t, "test", mapStr["type"])
+	assert.Equal(t, common.Time(now), mapStr["@timestamp"])
+}
+
+func TestExecEventToMapStrWIthEmptyFields(t *testing.T) {
 	event := ExecEvent{}
 	mapStr := event.ToMapStr()
 	_, fieldsExist := mapStr["fields"]
